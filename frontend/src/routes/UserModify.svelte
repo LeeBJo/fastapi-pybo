597b2c6240425a331029fc2f18b103375382166b
LeeBJo <qudwh0312@naver.com>
2022-11-29 22:55:49 +0900

    관리자 회원 추가 페이지 추가

--- frontend/src/routes/UserModify.svelte
+++ frontend/src/routes/UserModify.svelte
@@ -4,31 +4,33 @@
     import Error from "../components/Error.svelte"
 
     export let params = {}
-    const question_id = params.question_id          // 유저 이름
+    const username = params.username          // 유저 이름
 
     let error = {detail:[]}
     //유저의 속성들
-    let subject = ''
-    let content = ''
-    
+    let _username = ''
+    let email = ''
+    let alarmAccepted = false
+
     //유저 디테일로 변경
-    fastapi("get", "/api/question/detail/" + question_id, {}, (json) => {
-        subject = json.subject
-        content = json.content
+    fastapi("get", "/api/user/detail/" + username, {}, (json) => {
+        _username = json.username
+        email = json.email
+        alarmAccepted = alarmAccepted
     })
 
     //update_유저
-    function update_question(event) {
+    function update_user(event) {
         event.preventDefault()
-        let url = "/api/question/update"            // fastapi update 추가
+        let url = "/api/user/update"            // fastapi update 추가
         let params = {
-            question_id: question_id,
-            subject: subject,
-            content: content,
+            username: username,
+            email: email,
+            alarmAccepted: alarmAccepted,
         }
         fastapi('put', url, params, 
             (json) => {
-                push('/detail/'+question_id)
+                push('/detail/'+username)
             },
             (json_error) => {
                 error = json_error
@@ -41,14 +43,31 @@
     <h5 class="my-3 border-bottom pb-2">정보 수정</h5>
     <Error error={error} />
     <form method="post" class="my-3">
+        <!-- 유저 정보 표시-->
         <div class="mb-3">
-            <label for="subject">제목</label>
+            <label for="subject">이름</label>           <!-- 유저이름 -->
+            <input type="text" class="form-control" bind:value="{_username}">
+        </div>
+        <div class="mb-3">
+            <label for="subject">email</label>           <!-- 이메일 -->
+            <input type="text" class="form-control" bind:value="{email}">
+        </div>
+        <!-- 비밀번호 확인
+        <div class="mb-3">
+            <label for="subject">제목</label>           
             <input type="text" class="form-control" bind:value="{subject}">
         </div>
+         -->
+        <!-- 새 비밀번호
         <div class="mb-3">
-            <label for="content">내용</label>
-            <textarea class="form-control" rows="10" bind:value="{content}"></textarea>
+            <label for="subject">제목</label>            
+            <input type="text" class="form-control" bind:value="{subject}">
         </div>
-        <button class="btn btn-primary" on:click="{update_question}">수정하기</button>
+        -->
+        <div class="mb-3">
+            <label for="email">이메일 수신 동의 여부</label>        <!--이메일 수신 동의여부-->
+            <input type="checkbox"  id="alarmAccepted" bind:checked="{alarmAccepted}">
+        </div> 
+        <button class="btn btn-primary" on:click="{update_user}">수정하기</button>
     </form>
 </div>
